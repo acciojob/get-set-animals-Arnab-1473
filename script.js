74a2a6bcf80a33a5cf3628d6b138bb5557fc80cf
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-04-11 23:49:57 +0530

    updated

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal {
+    constructor(species) {
+        this.species = species;
+    }
+    get species() {
+        return this.species;
+    }
+     makeSound() {
+        console.log("The animal makes a sound");
+     }
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+    bark() {
+        console.log("woof");
+    }
+    makeSound() {
+        console.log("Bark");
+    }
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+    purr() {
+        console.log("purr");
+    }
+    makeSound() {
+        console.log("Meow");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
